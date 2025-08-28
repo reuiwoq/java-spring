6479dfc9d2db98a110c2fb47ecbad02f049621aa
reuiwoq <146545909+reuiwoq@users.noreply.github.com>
2025-08-28 17:12:12 +0900

    Add files via upload

---
+++ autoinject/service/MemberDao.java
@@ -0,0 +1,30 @@
+package com.abcdefg.service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.abcdefg.model.Member;
+
+public class MemberDao {
+	private static long nextId = 0;
+	
+	private Map<String, Member> map = new HashMap<>();
+
+	public Member selectByEmail(String email) {
+		return map.get(email);
+	}
+
+	public void insert(Member member) {
+		member.setId(++nextId);
+		map.put(member.getEmail(), member);
+	}
+
+	public void update(Member member) {
+		map.put(member.getEmail(), member);
+	}
+	
+	public Collection<Member> selectAll() {
+		return map.values();
+	}
+}
