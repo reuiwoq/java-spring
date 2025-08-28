554426d42bf072ccb34de58e98ef328ae039285e
reuiwoq <146545909+reuiwoq@users.noreply.github.com>
2025-08-28 17:29:19 +0900

    Add files via upload

---
+++ aoptest/config/ConcertConfig.java
@@ -0,0 +1,12 @@
+package com.abcdefg.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@ComponentScan(basePackages = {"com.abcdefg.concert"})
+public class ConcertConfig {
+	
+}
